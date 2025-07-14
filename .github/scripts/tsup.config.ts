b0db383b9e3edf454e8b0d87f6a5b64a305ad843
kotayam <ykomako4.9@gmail.com>
2025-07-14 10:01:14 +0900

    use tsup

---
+++ .github/scripts/tsup.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: [".github/scripts/pr-title-check/pr-title-check.ts"],
+    tsconfig: ".github/scripts/tsconfig.json",
+    dts: true,
+});
