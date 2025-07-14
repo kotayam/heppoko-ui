3e2fe801cf210ff21c5e71e4d8e5bf713a994be1
kotayam <ykomako4.9@gmail.com>
2025-07-14 10:03:14 +0900

    update yml

--- .github/scripts/tsup.config.ts
+++ .github/scripts/tsup.config.ts
@@ -3,5 +3,4 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     entry: [".github/scripts/pr-title-check/pr-title-check.ts"],
     tsconfig: ".github/scripts/tsconfig.json",
-    dts: true,
 });
