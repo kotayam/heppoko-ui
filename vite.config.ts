df02a0881422ea9995dde869a7ceda345a7e3358
kotayam <ykomako4.9@gmail.com>
2025-07-14 20:51:06 +0900

    setup cspell

--- vite.config.ts
+++ vite.config.ts
@@ -4,37 +4,37 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "HeppokoUI",
-      fileName: (format) => `heppoko-ui.${format}.js`,
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+    plugins: [react(), tsconfigPaths()],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.ts"),
+            name: "HeppokoUI",
+            fileName: (format) => `heppoko-ui.${format}.js`,
+            formats: ["es", "cjs"],
         },
-      },
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
     },
-  },
-  test: {
-    exclude: [...configDefaults.exclude, ".github"],
-    environment: "jsdom",
-    coverage: {
-      reporter: ["text", "json"],
-      include: ["src/"],
-      reportsDirectory: "coverage/report",
+    test: {
+        exclude: [...configDefaults.exclude, ".github"],
+        environment: "jsdom",
+        coverage: {
+            reporter: ["text", "json"],
+            include: ["src/"],
+            reportsDirectory: "coverage/report",
+        },
+        setupFiles: ["./vitest.setup.ts"],
     },
-    setupFiles: ["./vitest.setup.ts"],
-  },
-  css: {
-    modules: {
-      scopeBehaviour: "local",
+    css: {
+        modules: {
+            scopeBehaviour: "local",
+        },
     },
-  },
 });
