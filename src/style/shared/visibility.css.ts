f5eb2d3ce39fe81848b676d0dbdef9df3163d2f1
kotayam <ykomako4.9@gmail.com>
2025-08-01 15:44:08 -0400

    create variant for visibility

---
+++ src/style/shared/visibility.css.ts
@@ -0,0 +1,8 @@
+import { styleVariants } from "@vanilla-extract/css";
+
+export const visibilityVariants = styleVariants({
+    visible: { visibility: "visible" },
+    hidden: { visibility: "hidden" },
+});
+
+export type Visibility = keyof typeof visibilityVariants | (string & {});
