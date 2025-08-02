d340d18157fa6e61a44c22f1c27b16f04949e953
kotayam <ykomako4.9@gmail.com>
2025-08-01 22:26:25 -0400

    define props

--- src/props/main.props.ts
+++ src/props/main.props.ts
@@ -1,70 +1,76 @@
 import { CSSProperties } from "react";
 import {
-  BorderProps,
-  PositionProps,
-  PaddingProps,
-  MarginProps,
-  ShadowProps,
-  ColorProps,
+    PositionProps,
+    PaddingProps,
+    MarginProps,
+    ShadowProps,
+    ColorProps,
 } from "@/components/common";
 import { resolveVariants, VariantConfig } from "@/styles/utils/resolveVariants";
 import {
-  createVisibilityConfigs,
-  VisibilityProps,
+    createVisibilityConfigs,
+    VisibilityProps,
 } from "./shared/visibility.props";
 import {
-  createDimensionConfigs,
-  DimensionProps,
+    createDimensionConfigs,
+    DimensionProps,
 } from "./shared/dimension.props";
 import {
-  createOpacityConfigs,
-  OpacityProps,
+    createOpacityConfigs,
+    OpacityProps,
 } from "@/props/shared/opacity.props";
+import { BorderProps, createBorderConfigs } from "./shared/border.props";
 
 // combine all shared style props.
 export type StyleInputProps = DimensionProps &
-  BorderProps &
-  PositionProps &
-  PaddingProps &
-  MarginProps &
-  ShadowProps &
-  ColorProps &
-  OpacityProps &
-  VisibilityProps & {
-    style?: CSSProperties;
-    className?: string;
-  };
+    BorderProps &
+    PositionProps &
+    PaddingProps &
+    MarginProps &
+    ShadowProps &
+    ColorProps &
+    OpacityProps &
+    VisibilityProps & {
+        style?: CSSProperties;
+        className?: string;
+    };
 
 export const resolveStyleInput = (
-  props: StyleInputProps,
-  componentConfigs: VariantConfig[],
+    props: StyleInputProps,
+    componentConfigs: VariantConfig[],
 ) => {
-  // TODO: Add config creation for other shared props
-  const combinedConfigs = [
-    ...componentConfigs,
-    ...createDimensionConfigs(
-      props.width,
-      props.height,
-      props.minWidth,
-      props.minHeight,
-      props.maxWidth,
-      props.maxHeight,
-    ),
-    ...createOpacityConfigs(props.opacity),
-    ...createVisibilityConfigs(props.visibility),
-  ];
-  return resolveVariants(combinedConfigs, props.className, props.style);
+    // TODO: Add config creation for other shared props
+    const combinedConfigs = [
+        ...componentConfigs,
+        ...createDimensionConfigs(
+            props.width,
+            props.height,
+            props.minWidth,
+            props.minHeight,
+            props.maxWidth,
+            props.maxHeight,
+        ),
+        ...createBorderConfigs(
+            props.borderStyle,
+            props.borderWidth,
+            props.borderColor,
+            props.borderRadius,
+        ),
+        ...createOpacityConfigs(props.opacity),
+        ...createVisibilityConfigs(props.visibility),
+    ];
+    return resolveVariants(combinedConfigs, props.className, props.style);
 
-  // const combined = mergeStyles(
-  //     getDimensionStyle(props),
-  //     getBorderStyle(props),
-  //     getPositionStyle(props),
-  //     getPaddingStyle(props),
-  //     getMarginStyle(props),
-  //     getShadowStyle(props),
-  //     getColorStyle(props),
-  //     getOpacityStyle(props),
-  //     getVisibilityStyle(props),
-  //     style,
-  // );
+    // const combined = mergeStyles(
+    //     getDimensionStyle(props),
+    //     getBorderStyle(props),
+    //     getPositionStyle(props),
+    //     getPaddingStyle(props),
+    //     getMarginStyle(props),
+    //     getShadowStyle(props),
+    //     getColorStyle(props),
+    //     getOpacityStyle(props),
+    //     getVisibilityStyle(props),
+    //     style,
+    // );
 };
---
+++ src/props/shared/border.props.ts
@@ -0,0 +1,33 @@
+import {
+    BorderColor,
+    borderColorVariants,
+    BorderRadius,
+    borderRadiusVariants,
+    BorderStyle,
+    borderStyleVariants,
+    BorderWidth,
+    borderWidthVariants,
+} from "@/styles/shared/border.css";
+import { createConfig } from "@/styles/utils/resolveVariants";
+
+export type BorderProps = {
+    borderStyle?: BorderStyle;
+    borderWidth?: BorderWidth;
+    borderColor?: BorderColor;
+    borderRadius?: BorderRadius;
+};
+
+export const createBorderConfigs = (
+    borderStyle: BorderStyle | undefined,
+    borderWidth: BorderWidth | undefined,
+    borderColor: BorderColor | undefined,
+    borderRadius: BorderRadius | undefined,
+) => {
+    const configs = [
+        createConfig(borderStyleVariants, borderStyle, "borderStyle"),
+        createConfig(borderWidthVariants, borderWidth, "borderWidth"),
+        createConfig(borderColorVariants, borderColor, "borderColor"),
+        createConfig(borderRadiusVariants, borderRadius, "borderRadius"),
+    ];
+    return configs;
+};
