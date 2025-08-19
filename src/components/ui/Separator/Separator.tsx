f53f4f3fa62a4a6a111fdb63c13bee586cdf8c83
kotayam <ykomako4.9@gmail.com>
2025-08-19 15:33:29 -0400

    update component

--- src/components/ui/Separator/Separator.tsx
+++ src/components/ui/Separator/Separator.tsx
@@ -1,45 +1,22 @@
-import React, { CSSProperties } from "react";
-
-import { ColorProps, combineStyle, CustomStyleProps } from "../../common";
-import { OpacityProps } from "@/props/shared/opacity.props";
-import { MarginProps } from "@/props/shared/margin.props";
-
-type SeparatorProps = {
-  orientation?: "horizontal" | "vertical";
-  length?: CSSProperties["height"] | CSSProperties["width"];
-  thickness?: CSSProperties["height"] | CSSProperties["width"];
-} & CustomStyleProps &
-  MarginProps &
-  ColorProps &
-  OpacityProps;
+import {
+    createSeparatorConfigs,
+    SeparatorProps,
+} from "@/props/components/ui/separator.props";
+import { resolveStyleInput } from "@/props/main.props";
 
 export const Separator: React.FC<SeparatorProps> = ({
-  className,
-  orientation = "horizontal",
-  length = "100%",
-  thickness = "1px",
-  bg = "white",
-  opacity = 0.8,
-  m = "0.5rem",
-  ...rest
+    onClick,
+    orientation = "horizontal",
+    size = "full",
+    opacity = "translucent",
+    m = "0.5rem",
+    ...rest
 }) => {
-  const getSeparatorStyle = (): CSSProperties => {
-    if (orientation === "horizontal") {
-      return {
-        width: length,
-        height: thickness,
-      };
-    } else {
-      return { width: thickness, height: length };
-    }
-  };
-  const baseStyle: CSSProperties = {
-    ...getSeparatorStyle(),
-  };
-  const combinedStyle = {
-    ...baseStyle,
-    ...combineStyle({ bg, opacity, m, ...rest }),
-  };
-
-  return <div style={combinedStyle} className={className}></div>;
+    const res = resolveStyleInput(
+        { opacity: opacity, m: m, ...rest },
+        createSeparatorConfigs(orientation, size),
+    );
+    return (
+        <div className={res.className} style={res.style} onClick={onClick}></div>
+    );
 };
