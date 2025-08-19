42ed95aeb852760f8357ab2f4ea2799df797f5d4
kotayam <ykomako4.9@gmail.com>
2025-08-19 15:08:40 -0400

    update margin

--- src/props/shared/margin.props.ts
+++ src/props/shared/margin.props.ts
@@ -1,56 +1,66 @@
 import {
-  Margin,
-  MarginBottom,
-  marginBottomVariants,
-  MarginLeft,
-  marginLeftVariants,
-  MarginRight,
-  marginRightVariants,
-  MarginTop,
-  marginTopVariants,
-  marginVariants,
-  MarginX,
-  MarginY,
+    Margin,
+    MarginBottom,
+    marginBottomVariants,
+    MarginLeft,
+    marginLeftVariants,
+    MarginRight,
+    marginRightVariants,
+    MarginTop,
+    marginTopVariants,
+    marginVariants,
+    MarginX,
+    marginXVariants,
+    MarginY,
+    marginYVariants,
 } from "@/styles/shared/margin.css";
 import { createConfig } from "@/styles/utils/resolveVariants";
 
 export type MarginProps = {
-  m?: Margin;
-  mx?: MarginX;
-  my?: MarginY;
-  ml?: MarginLeft;
-  mr?: MarginRight;
-  mt?: MarginTop;
-  mb?: MarginBottom;
+    m?: Margin;
+    mx?: MarginX;
+    my?: MarginY;
+    ml?: MarginLeft;
+    mr?: MarginRight;
+    mt?: MarginTop;
+    mb?: MarginBottom;
 };
 
 export const createMarginConfigs = (
-  m: Margin | undefined,
-  mx: MarginX | undefined,
-  my: MarginY | undefined,
-  ml: MarginLeft | undefined,
-  mr: MarginRight | undefined,
-  mt: MarginTop | undefined,
-  mb: MarginBottom | undefined,
+    m: Margin | undefined,
+    mx: MarginX | undefined,
+    my: MarginY | undefined,
+    ml: MarginLeft | undefined,
+    mr: MarginRight | undefined,
+    mt: MarginTop | undefined,
+    mb: MarginBottom | undefined,
 ) => {
-  if (m) {
-    return [createConfig(marginVariants, m, "margin")];
-  }
-  const xConfigs = [
-    createConfig(marginLeftVariants, ml, "marginLeft"),
-    createConfig(marginRightVariants, mr, "marginRight"),
-  ];
-  if (mx) {
-    xConfigs[0] = createConfig(marginLeftVariants, mx, "marginLeft");
-    xConfigs[1] = createConfig(marginRightVariants, mx, "marginRight");
-  }
-  const yConfigs = [
-    createConfig(marginTopVariants, mt, "marginTop"),
-    createConfig(marginBottomVariants, mb, "marginBottom"),
-  ];
-  if (my) {
-    yConfigs[0] = createConfig(marginTopVariants, my, "marginTop");
-    yConfigs[1] = createConfig(marginBottomVariants, my, "marginBottom");
-  }
-  return [...xConfigs, ...yConfigs];
+    if (m) {
+        return [createConfig(marginVariants, m, "margin")];
+    }
+    let xConfigs = [
+        createConfig(marginLeftVariants, ml, "marginLeft"),
+        createConfig(marginRightVariants, mr, "marginRight"),
+    ];
+    if (mx) {
+        xConfigs = [
+            createConfig(marginXVariants, mx, {
+                marginLeft: mx,
+                marginRight: mx,
+            }),
+        ];
+    }
+    let yConfigs = [
+        createConfig(marginTopVariants, mt, "marginTop"),
+        createConfig(marginBottomVariants, mb, "marginBottom"),
+    ];
+    if (my) {
+        yConfigs = [
+            createConfig(marginYVariants, my, {
+                marginTop: my,
+                marginBottom: my,
+            }),
+        ];
+    }
+    return [...xConfigs, ...yConfigs];
 };
