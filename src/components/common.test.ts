d48bfb95700cc7ee1b771b95c273fbe9df63b8b0
kotayam <ykomako4.9@gmail.com>
2025-08-18 20:33:46 -0400

    update import in common test

--- src/components/common.test.ts
+++ src/components/common.test.ts
@@ -1,6 +1,6 @@
 import { cleanup } from "@testing-library/react";
 import { afterEach, describe, expect, it } from "vitest";
-import { ColorProps, combineStyle, getColor, PositionProps } from "./common";
+import { ColorProps, combineStyle, getColor } from "./common";
 import { VisibilityProps } from "@/props/shared/visibility.props";
 import { DimensionProps } from "@/props/shared/dimension.props";
 import { OpacityProps } from "@/props/shared/opacity.props";
@@ -8,86 +8,87 @@ import { BorderProps } from "@/props/shared/border.props";
 import { ShadowProps } from "@/props/shared/shadow.props";
 import { MarginProps } from "@/props/shared/margin.props";
 import { PaddingProps } from "@/props/shared/padding.props";
+import { PositionProps } from "@/props/shared/position.props";
 
 describe("Flex", () => {
-  afterEach(() => {
-    cleanup();
-  });
+    afterEach(() => {
+        cleanup();
+    });
 
-  const dimension: DimensionProps = {
-    width: "100%",
-    height: "100%",
-  };
-  const border: BorderProps = {
-    borderColor: "black",
-  };
+    const dimension: DimensionProps = {
+        width: "100%",
+        height: "100%",
+    };
+    const border: BorderProps = {
+        borderColor: "black",
+    };
 
-  const position: PositionProps = {
-    z: 2,
-  };
+    const position: PositionProps = {
+        z: 2,
+    };
 
-  const padding: PaddingProps = {
-    p: "1rem",
-    px: "2rem",
-    py: "3rem",
-  };
+    const padding: PaddingProps = {
+        p: "1rem",
+        px: "2rem",
+        py: "3rem",
+    };
 
-  const margin: MarginProps = {
-    m: "1rem",
-    mx: "2rem",
-    my: "3rem",
-  };
+    const margin: MarginProps = {
+        m: "1rem",
+        mx: "2rem",
+        my: "3rem",
+    };
 
-  const shadow: ShadowProps = {
-    shadow: "md",
-  };
+    const shadow: ShadowProps = {
+        shadow: "md",
+    };
 
-  const opacity: OpacityProps = {
-    opacity: 0.75,
-  };
+    const opacity: OpacityProps = {
+        opacity: 0.75,
+    };
 
-  const visibility: VisibilityProps = {
-    visibility: "hidden",
-  };
+    const visibility: VisibilityProps = {
+        visibility: "hidden",
+    };
 
-  const color: ColorProps = {
-    color: "red",
-    bg: "blue",
-    stroke: "green",
-  };
+    const color: ColorProps = {
+        color: "red",
+        bg: "blue",
+        stroke: "green",
+    };
 
-  const styleInput = {
-    ...dimension,
-    ...border,
-    ...position,
-    ...padding,
-    ...margin,
-    ...shadow,
-    ...opacity,
-    ...visibility,
-    ...color,
-  };
+    const styleInput = {
+        ...dimension,
+        ...border,
+        ...position,
+        ...padding,
+        ...margin,
+        ...shadow,
+        ...opacity,
+        ...visibility,
+        ...color,
+    };
 
-  it("custom names are converted to css properties", () => {
-    const style = combineStyle(styleInput);
-    expect(style.zIndex).toBe(2);
-    expect(style.padding).toBe("1rem");
-    expect(style.paddingTop).toBeUndefined();
-    expect(style.paddingBottom).toBeUndefined();
-    expect(style.margin).toBe("1rem");
-    expect(style.marginTop).toBeUndefined();
-    expect(style.marginBottom).toBeUndefined();
-    expect(style.boxShadow).toBeDefined();
-    expect(style.borderColor).toBe(getColor("black"));
-    expect(style.color).toBe(getColor("red"));
-    expect(style.backgroundColor).toBe(getColor("blue"));
-    expect(style.stroke).toBe(getColor("green"));
-  });
+    it("custom names are converted to css properties", () => {
+        const style = combineStyle(styleInput);
+        expect(style.zIndex).toBe(2);
+        expect(style.padding).toBe("1rem");
+        expect(style.paddingTop).toBeUndefined();
+        expect(style.paddingBottom).toBeUndefined();
+        expect(style.margin).toBe("1rem");
+        expect(style.marginTop).toBeUndefined();
+        expect(style.marginBottom).toBeUndefined();
+        expect(style.boxShadow).toBeDefined();
+        expect(style.borderColor).toBe(getColor("black"));
+        expect(style.color).toBe(getColor("red"));
+        expect(style.backgroundColor).toBe(getColor("blue"));
+        expect(style.stroke).toBe(getColor("green"));
+    });
 
-  it("undefined values are filtered from output", () => {
-    const style = combineStyle(styleInput);
-    for (const value of Object.values(style)) {
-      expect(value).toBeDefined();
-    }
-  });
+    it("undefined values are filtered from output", () => {
+        const style = combineStyle(styleInput);
+        for (const value of Object.values(style)) {
+            expect(value).toBeDefined();
+        }
+    });
 });
