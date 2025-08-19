162136553b7b067f87a8a0565c6f425024e54141
kotayam <ykomako4.9@gmail.com>
2025-08-18 20:32:27 -0400

    create props

---
+++ src/props/shared/position.props.ts
@@ -0,0 +1,48 @@
+import {
+    Bottom,
+    bottomVariants,
+    Inset,
+    insetVariants,
+    Left,
+    leftVariants,
+    Position,
+    positionVariants,
+    Right,
+    rightVariants,
+    Top,
+    topVariants,
+    ZIndex,
+    zIndexVariants,
+} from "@/styles/shared/position.css";
+import { createConfig } from "@/styles/utils/resolveVariants";
+
+export type PositionProps = {
+    position?: Position;
+    inset?: Inset;
+    top?: Top;
+    bottom?: Bottom;
+    left?: Left;
+    right?: Right;
+    z?: ZIndex;
+};
+
+export const createPositionConfigs = (
+    position: Position | undefined,
+    inset: Inset | undefined,
+    top: Top | undefined,
+    bottom: Bottom | undefined,
+    left: Left | undefined,
+    right: Right | undefined,
+    z: ZIndex | undefined,
+) => {
+    const configs = [
+        createConfig(positionVariants, position, "position"),
+        createConfig(insetVariants, inset, "inset"),
+        createConfig(topVariants, top, "top"),
+        createConfig(bottomVariants, bottom, "bottom"),
+        createConfig(leftVariants, left, "left"),
+        createConfig(rightVariants, right, "right"),
+        createConfig(zIndexVariants, z, "zIndex"),
+    ];
+    return configs;
+};
